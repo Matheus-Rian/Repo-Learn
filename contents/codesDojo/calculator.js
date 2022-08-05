57848b3216645f4cb858da98df8fa7c93b6b7ef6
Matheus Rian <souzamatheusrian@gmail.com>
2022-08-04 22:12:38 -0300

    feat: code dojo calculator

---
+++ contents/codesDojo/calculator.js
@@ -0,0 +1,11 @@
+const operations = {
+  sum: (x, y) => x + y,
+  sub: (x, y) => x - y,
+  div: (x, y) => x / y,
+  mult: (x, y) => x * y,
+}
+
+console.log(operations.sum(2, 2));
+console.log(operations.sub(2, 2));
+console.log(operations.div(2, 2));
+console.log(operations.mult(2, 2));

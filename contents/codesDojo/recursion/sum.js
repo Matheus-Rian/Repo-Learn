1c8340171e36aa9162d0b2379a5b99564976e8be
Matheus Rian <souzamatheusrian@gmail.com>
2022-11-30 20:53:55 -0300

    feat: created alternative function sum recursion

--- contents/codesDojo/recursion/sum.js
+++ contents/codesDojo/recursion/sum.js
@@ -7,3 +7,12 @@ function sum(numbers) {
 }
 
 console.log(sum([1, 2, 3, 6]));
+
+// Other Solution
+
+function sumRecursive(arr) {
+  if (arr.length == 0) return 0;
+  return arr[0] + sumRecursive(arr.slice(1));
+}
+
+console.log(sumRecursive([1, 2, 3, 4])); // 10
